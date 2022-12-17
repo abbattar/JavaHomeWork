2f36ce3f175431679fff5ff99bafe74ab70b07b8
abbattar <abbattar@mail.ru>
2022-12-17 22:19:34 +0300

    Logger ready

--- Home_Work_2/JHW2_2.java
+++ Home_Work_2/JHW2_2.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.logging.FileHandler;
 import java.util.logging.SimpleFormatter;
-import java.util.stream.Collectors;
-//import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.util.logging.Level;
 
@@ -20,18 +18,21 @@ public class JHW2_2 {
             array[i] = (int)(Math.random()*100);
             System.out.println(array[i]);
         }
-
-        int[] sortedArray = Bubble(array);
-        for (int item : sortedArray) {
-            System.out.printf("%d ", item);
-        }
-    }
-
-    public static int[] Bubble(int[] array) throws SecurityException, IOException {
         Logger logger = Logger.getLogger(JHW2_2.class.getName());
         logger.setLevel(Level.INFO);
         FileHandler fh = new FileHandler("log.log");
         fh.setFormatter(new SimpleFormatter());
+        logger.addHandler(fh);
+
+        int[] sortedArray = Bubble(array);
+        for (int item : sortedArray) {
+            System.out.printf("%d ", item);
+            logger.log(Level.INFO, "+ 1");
+        }
+    }
+
+
+    public static int[] Bubble(int[] array) throws SecurityException, IOException {
         int currentPosition;
         int maxPosition;
         int temp;
@@ -39,7 +40,7 @@ public class JHW2_2 {
         int compareCount = 0;
         long time = System.nanoTime();
 
-        for (maxPosition=array.length - 1; maxPosition >= 0;maxPosition--)	{
+        for (maxPosition = array.length - 1; maxPosition >= 0; maxPosition--)	{
             for (currentPosition = 0; currentPosition < maxPosition; currentPosition++) {
                 compareCount++;
                 if (array[currentPosition] > array[currentPosition+1]) {
@@ -47,13 +48,13 @@ public class JHW2_2 {
                     array[currentPosition] = array[currentPosition+1];
                     array[currentPosition+1] = temp;
                     switchCount++;
-                    logger.addHandler(fh);
                 }
             }
         }
     time = System.nanoTime() - time;
     return array;
-}
+    }
+
     public static boolean validate(int[] array, int[] sortedArray) {
         if (Arrays.equals(array, sortedArray)) {
             return false;
